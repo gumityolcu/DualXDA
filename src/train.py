0e9d99e517269df7188d0105bf07add2e77cbf65
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-07-16 14:06:57 +0200

    fix bug

--- src/train.py
+++ src/train.py
@@ -44,7 +44,7 @@ def test_models_in_dir(model_root_path, model_name, device, num_classes, class_g
         dataset_type=fname.split("_")[-1]
         num_classes=5 if dataset_type=="group" else 10 
         train_acc=evaluate_model(
-            model_name=model_name,
+            model=model_name,
             device=device,
             num_classes=num_classes,
             class_groups=class_groups,
@@ -58,7 +58,7 @@ def test_models_in_dir(model_root_path, model_name, device, num_classes, class_g
             image_set="train"
         )
         test_acc = evaluate_model(
-            model_name=model_name,
+            model=model_name,
             device=device,
             num_classes=num_classes,
             class_groups=class_groups,
@@ -338,7 +338,7 @@ def start_training(model_name, device, num_classes, class_groups, data_root, epo
 
             print(f"\n\nValidation loss: {validation_loss}\n\n")
             writer.add_scalar('Loss/val', validation_loss, base_epoch + e)
-            valeval = evaluate_model(model_name=model_name, device=device, num_classes=num_classes,
+            valeval = evaluate_model(model=model, device=device, num_classes=num_classes,
                                      data_root=data_root,
                                      batch_size=batch_size, num_batches_to_process=num_batches_eval,
                                      load_path=model_save_path, dataset_name=dataset_name, dataset_type=dataset_type,
@@ -368,16 +368,18 @@ def start_training(model_name, device, num_classes, class_groups, data_root, epo
     writer.close()
     save_id = os.path.basename(best_model_yet)
 
-def evaluate_model(model_name, device, num_classes, class_groups, data_root, batch_size,
+def evaluate_model(model, device, num_classes, class_groups, data_root, batch_size,
                    num_batches_to_process, load_path, dataset_name, dataset_type, validation_size, image_set):
     if not torch.cuda.is_available():
         device="cpu"
-    if dataset_name == 'CIFAR':
-        model = load_cifar_model(model_name, dataset_name, num_classes, device=device, train=True)
-    elif dataset_name == 'AWA':
-        model = load_awa_model(model_name, dataset_name, num_classes, device=device, train=True)
-    else:
-        model = load_model(model_name, dataset_name, num_classes).to(device)
+    if isinstance(model,str):
+        model_name=model
+        if dataset_name == 'CIFAR':
+            model = load_cifar_model(model_name, dataset_name, num_classes, device=device, train=True)
+        elif dataset_name == 'AWA':
+            model = load_awa_model(model_name, dataset_name, num_classes, device=device, train=True)
+        else:
+            model = load_model(model_name, dataset_name, num_classes).to(device)
 
     kwparams = {
         'data_root': data_root,
@@ -392,9 +394,10 @@ def evaluate_model(model_name, device, num_classes, class_groups, data_root, bat
         return 0.0, 0.0
     loader = DataLoader(ds, batch_size=batch_size, shuffle=True)
 
-    if load_path is not None:
-        checkpoint = torch.load(load_path, map_location=device)
-        model.load_state_dict(checkpoint["model_state"])
+    if isinstance(model, str):
+        if load_path is not None:
+            checkpoint = torch.load(load_path, map_location=device)
+            model.load_state_dict(checkpoint["model_state"])
     # classes = ds.all_classes
     model.eval()
     y_true = torch.empty(0, device=device)
@@ -435,7 +438,7 @@ def test_all_models(model_root_path, model_name, device, num_classes, class_grou
         dataset_type=dir_name
         num_classes=5 if dataset_type=="group" else 10
         train_acc=evaluate_model(
-            model_name=model_name,
+            model=model_name,
             device=device,
             num_classes=num_classes,
             class_groups=class_groups,
@@ -449,7 +452,7 @@ def test_all_models(model_root_path, model_name, device, num_classes, class_grou
             image_set="train"
         )
         test_acc = evaluate_model(
-            model_name=model_name,
+            model=model_name,
             device=device,
             num_classes=num_classes,
             class_groups=class_groups,
