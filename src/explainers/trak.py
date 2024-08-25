9222fb9ba63089c11d9f150bae69a3e2816d866c
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-08-25 23:05:14 +0200

    fix trak featurization issue

--- src/explainers/trak.py
+++ src/explainers/trak.py
@@ -28,7 +28,7 @@ class TRAK(Explainer):
         self.traker.load_checkpoint(self.model.state_dict(),model_id=0)
         for (i,(x,y)) in enumerate(iter(ld)):
             batch=x.to(self.device), y.to(self.device)
-            self.traker.featurize(batch=batch,inds=torch.tensor([i*self.batch_size+j for j in range(self.batch_size)]))
+            self.traker.featurize(batch=batch,inds=torch.tensor([i*self.batch_size+j for j in range(min(self.batch_size,len(self.dataset)))]))
         self.traker.finalize_features()
 
     def explain(self, x, xpl_targets):
