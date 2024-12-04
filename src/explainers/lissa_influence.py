make self_influence computation exclusive to 'corrupt' experiments, fix lissa self influence computation. only need to finalize trak before merge

src/explainers/lissa_influence.py
@@ -324,13 +324,34 @@ class BaseInfluenceModule(abc.ABC):
     # Loss and autograd helpers
 
     def _loss_grad_loader_wrapper(self, train, **kwargs):
-        params = self._model_params(with_names=False)
+        params = self._model_params(with_names=False)        
         flat_params = self._flatten_params_like(params)
 
         for batch, batch_size in self._loader_wrapper(train=train, **kwargs):
-            loss_fn = self.objective.train_loss if train else self.objective.test_loss
-            loss = loss_fn(model=self.model, params=flat_params, batch=batch)
-            yield self._flatten_params_like(torch.autograd.grad(loss, params)), batch_size
+            # loss_fn = self.objective.train_loss if train else self.objective.test_loss
+            # loss = loss_fn(model=self.model, params=flat_params, batch=batch)
+            # yield self._flatten_params_like(torch.autograd.grad(loss, params)), batch_size
+
+            #=========================            
+            #v2
+            outputs = self.objective.train_outputs(self.model, batch)
+            if train:
+                loss=self.objective.train_loss_on_outputs(outputs, batch) + self.objective.train_regularization(params)
+            else:
+                loss=self.objective.test_loss(self.model, params, batch)
+            loss.backward()
+            param_grads = tuple(p.grad for p in self._model_params(with_names=False))
+            assert all([p is not None for p in param_grads])
+            assert all([not torch.isclose(p, torch.zeros_like(p)).all() for p in param_grads]) or torch.isclose(loss,torch.tensor(0.))
+            yield self._flatten_params_like(param_grads), batch_size
+            #=========================
+            #v3
+            # loss=cross_entropy(self.model(batch[0]),batch[1])
+            # loss.backward()
+            # param_grads = tuple(p.grad for _, p in self.model.influence_named_parameters())
+            # assert all([p is not None for p in param_grads])
+            # assert all([not torch.isclose(p, torch.zeros_like(p)).all() for p in param_grads]) or torch.isclose(loss,torch.tensor(0.))
+            # yield self._flatten_params_like(param_grads), batch_size
 
 
     def _sample_loss_grad_loader_wrapper(self, x,targets,train):
@@ -443,7 +464,7 @@ class LiSSAInfluenceFunctionExplainer(Explainer):
     name = "LiSSAInfluenceFunctionExplainer"
 
     def __init__(self, model, dataset, device, depth, repeat, dir, train_loss=cross_entropy,
-                 train_regularization=(lambda x: 0), test_loss=cross_entropy):
+                 train_regularization=(lambda x: torch.tensor(0., device=x[0].device if x is not None else "cpu")), test_loss=cross_entropy):
         class MyObjective(BaseObjective):
             def train_outputs(self, model, batch):
                 return model(batch[0])
@@ -472,7 +493,6 @@ class LiSSAInfluenceFunctionExplainer(Explainer):
                                                      depth=depth, repeat=repeat, scale=1.0, damp=0.001, device=device)
 
     def train(self):
-        self.self_influences()
         return 0.
 
     def explain(self, x, xpl_targets):

src/explainers/trak.py
@@ -49,11 +49,26 @@ class TRAK(Explainer):
         return torch.from_numpy(self.traker.finalize_scores(exp_name='test')).T.to(self.device)
 
     def self_influences(self):
-        raise NotImplementedError
         if os.path.exists(os.path.join(self.dir, "self_influences")):
             self_inf=torch.load(os.path.join(self.dir, "self_influences"))
         else:
             self_inf=torch.zeros((len(self.dataset),), device=self.device)
-            pass # TODO implement self_influences
+            self.compute_self_influences_brute_force()
             torch.save(self_inf, os.path.join(self.dir, "self_influences"))
         return self_inf
+    
+    def compute_self_influences_brute_force(self):
+        pass # TODO : implement self-influences and caching
+        # ld=torch.utils.data.DataLoader(self.dataset, batch_size=self.batch_size)
+        
+        # for (i,(x,y)) in enumerate(iter(ld)):
+        #     self.traker.start_scoring_checkpoint(model_id=0,
+        #                                      checkpoint=self.model.state_dict(),
+        #                                      exp_name=f'self_influences_{i}',
+        #                                     num_targets=x.shape[0])
+        #     self.traker.score(batch=(x,y), num_samples=x.shape[0])
+        #     xpl = torch.from_numpy(self.traker.finalize_scores(exp_name=f'self_influences_{i}')).T.to(self.device)
+        #     batch=x.to(self.device), y.to(self.device)
+        #     self.traker.score(batch=batch, num_samples=min(self.batch_size,len(self.dataset)-i*self.batch_size))
+        #     self_inf[i*self.batch_size:i*self.batch_size+min(self.batch_size,len(self.dataset)-i*self.batch_size)]=torch.from_numpy(self.traker.finalize_scores(exp_name='test')).T.to(self.device).diag()
+        # return self_inf

src/utils/__init__.py
@@ -44,13 +44,16 @@ def zennit_inner_product_explanation(model, train, test, composite_cls=EpsilonAl
 
 
 def xplain(model, train, test, device, explainer_cls, batch_size, kwargs, num_batches_per_file, save_dir,
-           start_file, num_files, graddot=False):
+           start_file, num_files, graddot=False, self_influence=False):
     torch.manual_seed(42)
     # the graddot parameter indicates if we are generating graddot attributions
     # if it is true, graddot.explain will be called a second time with normalize=True to generate gradcos along the way
 
     explainer = explainer_cls(model=model, dataset=train, device=device, **kwargs)
     explainer.train()
+    if self_influence:
+        explainer.self_influences()
+        exit()
 
     test_ld = DataLoader(test, batch_size=batch_size, shuffle=False)
     explanations = torch.empty((0, len(train)), device=device)

src/utils/models.py
@@ -31,7 +31,7 @@ class ResNetWrapper(torch.nn.Module):
         return self.classifier(x)
 
     def influence_named_parameters(self):
-       return [("classifier.weight", self.classifier.weight)]
+       return [("classifier.weight", self.classifier.weight), ("classifier.bias", self.classifier.bias)]
 
     def sim_parameters(self):
         return self.parameters()