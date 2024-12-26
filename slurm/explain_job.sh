d978d21e79b7be6d3d410b95762a7cbe2befbe0a
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-12-26 22:09:10 +0100

    change lissa param again

--- slurm/explain_job.sh
+++ slurm/explain_job.sh
@@ -14,13 +14,6 @@ start=`date +%s`
 
 mkdir -p ${LOCAL_JOB_DIR}/outputs
 
-tar -czf ${LOCAL_JOB_DIR}/config_files.tgz ${HOME}/DualView/config_files
-tar -czf ${LOCAL_JOB_DIR}/checkpoints.tgz ${HOME}/DualView/checkpoints
-
-echo "TAR DONE"
-tar -C ${LOCAL_JOB_DIR} -zxf ${LOCAL_JOB_DIR}/config_files.tgz home/fe/yolcu/DualView/config_files --strip-components=4
-tar -C ${LOCAL_JOB_DIR} -zxf ${LOCAL_JOB_DIR}/checkpoints.tgz home/fe/yolcu/DualView/checkpoints --strip-components=4
-
 fname_config=$(basename "$1")
 config_name=${fname_config::-5}
 
@@ -28,8 +21,8 @@ if [[ "$fname_config" == *"trak"* ]]; then
   singularity \
   run \
         --nv \
-        --bind ${LOCAL_JOB_DIR}/config_files:/mnt/config_files \
-        --bind ${LOCAL_JOB_DIR}/checkpoints:/mnt/checkpoints \
+        --bind ${HOME}/DualView/config_files:/mnt/config_files \
+        --bind ${HOME}/DualView/checkpoints:/mnt/checkpoints \
         --bind ${DATAPOOL3}/datasets:/mnt/dataset \
         --bind ${LOCAL_JOB_DIR}/outputs:/mnt/outputs \
         --bind ${HOME}/DualView/cache:/mnt/cache \
@@ -38,18 +31,16 @@ else
   singularity \
     run \
           --nv \
-          --bind ${LOCAL_JOB_DIR}/config_files:/mnt/config_files \
-          --bind ${LOCAL_JOB_DIR}/checkpoints:/mnt/checkpoints \
+          --bind ${HOME}/DualView/config_files:/mnt/config_files \
+          --bind ${HOME}/DualView/checkpoints:/mnt/checkpoints \
           --bind ${DATAPOOL3}/datasets:/mnt/dataset \
           --bind ${LOCAL_JOB_DIR}/outputs:/mnt/outputs \
-          --bind ${HOME}/cache:/mnt/cache \
+          --bind ${HOME}/DualView/cache:/mnt/cache \
           ../singularity/explain.sif --config_file /mnt/config_files/cluster/$2/$3/${fname_config}
 fi
 cd ${LOCAL_JOB_DIR}
 tar -czf $3_$2_${fname_config}-output_data_${SLURM_JOB_ID}.tgz outputs
 cp $3_$2_${fname_config}-output_data_${SLURM_JOB_ID}.tgz ${SLURM_SUBMIT_DIR}
-
-rm -rf ${LOCAL_JOB_DIR}/*
 
 end=`date +%s`
 runtime=$((end-start))
