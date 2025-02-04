5f3ea40a21dc4b2ddd24dc223bc12caf699e88fc
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-02-04 02:23:23 +0100

    minor fix

--- slurm/evaluate.sh
+++ slurm/evaluate.sh
@@ -1,4 +1,4 @@
-for file in ../config_files/cluster/explain/$1/*$2*.yaml; do
+for file in ../config_files/cluster/evaluate/$1/*$2*.yaml; do
     echo $file
     sbatch evaluate_job.sh $file $1
 done;
