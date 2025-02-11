38744c36b5c2337635bafb90ee5b841ad372e981
Moritz Weckbecker <moritz.weckbecker@hhi.fraunhofer.de>
2025-02-11 16:14:53 +0100

    Updated same class metric

--- src/metrics/same_class_topk_metrics.py
+++ src/metrics/same_class_topk_metrics.py
@@ -30,8 +30,8 @@ class TopKSameClassMetric(Metric):
         else:
             topk_idx = xpl.topk(self.k)[1]
             for i in range(topk_idx.shape[0]):
-                topk_most_influential_labels = self.train[topk_idx[i].to('cpu')][1]
-                test_label = self.test[start_index+i][1].repeat(self.k, 1)
+                topk_most_influential_labels = torch.Tensor([self.train[topk_idx[i, j]][1] for j in range(self.k)]).to('cpu')
+                test_label = self.test[start_index+i][1].repeat(self.k, 1).to('cpu')
                 is_equal = torch.mean((test_label == topk_most_influential_labels) * 1., axis=0)
                 self.scores = torch.cat((self.scores, torch.tensor(is_equal).to(self.device)), dim=0)
 
