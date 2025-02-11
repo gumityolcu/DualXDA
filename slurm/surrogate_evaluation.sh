f9ff289bd225d3afbe1db144203b02f39855e074
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-02-11 20:02:09 +0100

    fix slurm

--- slurm/surrogate_evaluation.sh
+++ slurm/surrogate_evaluation.sh
@@ -1,4 +1,4 @@
-for file in ../config_files/cluster/evaluate/$1/*$2*.yaml; do
+for file in ../config_files/cluster/surrogate_evaluation/$1/*$2*.yaml; do
     echo $file
     sbatch surrogate_evaluation_job.sh $file $1
 done;
