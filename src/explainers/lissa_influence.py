b1b8180b6e21b2a0fa7eb46abb43ee67b2170f71
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-02-19 20:29:25 +0100

    add log prints

--- src/explainers/lissa_influence.py
+++ src/explainers/lissa_influence.py
@@ -231,7 +231,6 @@ class BaseInfluenceModule(abc.ABC):
         print("LOG:starting self influences")
         for ((batch, _),(grad_z, _)) in zip(train_sample_loader,train_grad_loader):
             (x, targets) = batch
-            print("LOG: runnign self influence")
             i=i+1
             t0=time()
             stest = self.stest(x,targets)
@@ -240,7 +239,7 @@ class BaseInfluenceModule(abc.ABC):
             t=time()-t0
             avg=avg+t
             if i%100==0:
-                print(f"{i}/{len(self.train_loader.dataset)}") 
+                print(f"LOG: Iteration {i}/{len(self.train_loader.dataset)}") 
                 print(torch.any(torch.isnan(s)))
                 print(avg/100.)
                 print("=====")
