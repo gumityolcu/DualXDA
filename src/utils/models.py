eae8187c8dbcf1b9eed4cc4720a74f1663c84355
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-02-28 14:23:34 +0100

    reduce parameters even more

--- src/utils/models.py
+++ src/utils/models.py
@@ -46,7 +46,7 @@ class ResNetWrapper(torch.nn.Module):
     def arnoldi_parameters(self):
         if self.arnoldi_param_filter is None:           
             return None #None means we use all parameters. This is needed for full model explanation with CIFAR
-        return ["classifier", "features.7.1", "features.7.0.conv3", "features.7.0.bn3", "features.7.0.downsample"]
+        return ["classifier", "features.7.1"]
          
 
     def sim_parameters(self):
