d94ee2e1be0e1bc5cbbe56a14c8fb5301415cffc
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-03-03 22:44:39 +0100

    fix

--- src/metrics/mark_metric.py
+++ src/metrics/mark_metric.py
@@ -20,8 +20,13 @@ class MarkImageMetric(Metric):
             if not self.filter:
                 return True
             return (torch.tensor(self.test[i][1],device=self.device)==self.marked_cls) and (torch.tensor(self.test.dataset[i][1],device=self.device)!=self.marked_cls)
-        passed = [1. if most_influential_ids[i] in self.marked_samples else 0.
-            for i in range(xpl.shape[0]) if include_datapoint(i)]
+        passed = []
+        for i in range(xpl.shape[0]):
+            if include_datapoint(i):
+                if most_influential_ids[i] in self.marked_samples:
+                    passed.append(1.)
+                else:
+                    passed.append(0.)
         passed = torch.tensor(passed, device=self.device)
         self.scores = torch.cat((self.scores, passed), dim=0)
         
