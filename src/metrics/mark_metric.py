025aac3b4411a9a70ddeff1a10fb169b02b6ec67
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-03-06 18:05:30 +0100

    fix mark

--- src/metrics/mark_metric.py
+++ src/metrics/mark_metric.py
@@ -16,14 +16,16 @@ class MarkImageMetric(Metric):
 
     def __call__(self, xpl, start_index):
         xpl=xpl.to(self.device)
-        most_influential_ids = xpl.argsort(dim=-1)[:,:self.topk]
+        most_influential_ids = torch.argsort(-xpl, dim=-1)[:,:self.topk]
         self.marked_samples=torch.tensor(self.marked_samples).to(self.device)
+        cnt=0
         def include_datapoint(i):
             if not self.filter:
                 return True
             return (torch.tensor(self.test[i][1],device=self.device)==self.marked_cls) and (torch.tensor(self.test.dataset[i][1],device=self.device)!=self.marked_cls)
         new_scores = []
         for i in range(xpl.shape[0]):
+            cnt=cnt+1
             if include_datapoint(i):
                 passed=[]
                 for id in most_influential_ids[i]:
@@ -34,7 +36,8 @@ class MarkImageMetric(Metric):
                 new_scores.append(torch.tensor(passed).mean())
         new_scores = torch.tensor(new_scores, device=self.device)
         self.scores = torch.cat((self.scores, new_scores), dim=0)
-        
+
+
 
     def get_result(self, dir, file_name):
         self.scores = self.scores.to('cpu').numpy()
