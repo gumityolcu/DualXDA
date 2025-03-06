30eed2977845ee4c3f1337029df7fa85f73606dc
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-03-06 17:25:25 +0100

    switch mark metric to top k

--- src/metrics/mark_metric.py
+++ src/metrics/mark_metric.py
@@ -4,32 +4,36 @@ import torch
 class MarkImageMetric(Metric):
     name = "MarkImageMetric"
 
-    def __init__(self, train, test, model, device="cuda", filter=True):
+    def __init__(self, train, test, model, device="cuda", filter=True, topk=1):
         self.marked_cls = torch.tensor(train.cls_to_mark,device=device)
         self.filter=filter
         self.marked_samples = train.mark_samples
         self.train = train
         self.test = test 
+        self.topk = topk
         self.scores = torch.empty(0, dtype=torch.float, device=device)
         self.device = device
 
     def __call__(self, xpl, start_index):
         xpl=xpl.to(self.device)
-        most_influential_ids = xpl.argmax(axis=-1)
+        most_influential_ids = xpl.argsort(dim=-1)[:,:self.topk]
         self.marked_samples=torch.tensor(self.marked_samples).to(self.device)
         def include_datapoint(i):
             if not self.filter:
                 return True
             return (torch.tensor(self.test[i][1],device=self.device)==self.marked_cls) and (torch.tensor(self.test.dataset[i][1],device=self.device)!=self.marked_cls)
-        passed = []
+        new_scores = []
         for i in range(xpl.shape[0]):
             if include_datapoint(i):
-                if most_influential_ids[i] in self.marked_samples:
-                    passed.append(1.)
-                else:
-                    passed.append(0.)
-        passed = torch.tensor(passed, device=self.device)
-        self.scores = torch.cat((self.scores, passed), dim=0)
+                passed=[]
+                for id in most_influential_ids[i]:
+                    if id in self.marked_samples:
+                        passed.append(1.)
+                    else:
+                        passed.append(0.)
+                new_scores.append(torch.tensor(passed).mean())
+        new_scores = torch.tensor(new_scores, device=self.device)
+        self.scores = torch.cat((self.scores, new_scores), dim=0)
         
 
     def get_result(self, dir, file_name):
