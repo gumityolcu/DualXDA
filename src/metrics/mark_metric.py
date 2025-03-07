443a198e28f0abb53c77c64645c00466521f78a7
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-03-07 01:47:48 +0100

    add binary auprc to mark metric

--- src/metrics/mark_metric.py
+++ src/metrics/mark_metric.py
@@ -1,5 +1,7 @@
 from utils import Metric
 import torch
+from torcheval.metrics.functional import binary_auprc
+
 
 class MarkImageMetric(Metric):
     name = "MarkImageMetric"
@@ -7,35 +9,31 @@ class MarkImageMetric(Metric):
     def __init__(self, train, test, model, device="cuda", filter=True, topk=1):
         self.marked_cls = torch.tensor(train.cls_to_mark,device=device)
         self.filter=filter
-        self.marked_samples = train.mark_samples
         self.train = train
         self.test = test 
         self.topk = topk
         self.scores = torch.empty(0, dtype=torch.float, device=device)
         self.device = device
+        self.binary_shortcut_indices: torch.Tensor = torch.tensor(
+            [
+                1 if i in self.train.mark_samples else 0
+                for i in range(len(self.train))
+            ],
+            device=self.device,
+        )
+
 
     def __call__(self, xpl, start_index):
         xpl=xpl.to(self.device)
-        most_influential_ids = torch.argsort(-xpl, dim=-1)[:,:self.topk]
-        self.marked_samples=torch.tensor(self.marked_samples).to(self.device)
+        #most_influential_ids = torch.argsort(-xpl, dim=-1)[:,:self.topk]
         cnt=0
         def include_datapoint(i):
             if not self.filter:
                 return True
             return (torch.tensor(self.test[i][1],device=self.device)==self.marked_cls) and (torch.tensor(self.test.dataset[i][1],device=self.device)!=self.marked_cls)
-        new_scores = []
-        for i in range(xpl.shape[0]):
-            cnt=cnt+1
-            if include_datapoint(i):
-                passed=[]
-                for id in most_influential_ids[i]:
-                    if id in self.marked_samples:
-                        passed.append(1.)
-                    else:
-                        passed.append(0.)
-                new_scores.append(torch.tensor(passed).mean())
-        new_scores = torch.tensor(new_scores, device=self.device)
-        self.scores = torch.cat((self.scores, new_scores), dim=0)
+        filter_indices=[include_datapoint(i) for i in range(xpl.shape[0])]   
+        expanded_binary_indices=self.binary_shortcut_indices.expand((xpl.shape[0], len(self.train)))     
+        self.scores = torch.cat((self.scores,  binary_auprc(xpl, expanded_binary_indices, num_tasks=xpl.shape[0])), dim=0)
 
 
 
