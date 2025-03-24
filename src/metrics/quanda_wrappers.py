566611457d578f55cb642c443a5c99497cad3352
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-03-24 13:44:11 +0100

    quanda_wrappers added

---
+++ src/metrics/quanda_wrappers.py
@@ -0,0 +1,78 @@
+from quanda.metrics.ground_truth import LinearDatamodelingMetric
+import torch
+import os
+from metrics import Metric
+
+class QuandaLDSWrapper(Metric):
+    name = "QuandaLDS"
+
+    def process_cache_dir(self, pretrained_models):
+        subset_indices=[]
+        subset_fpath=os.path.join(self.cache_dir, "subset_indices")
+        if not os.path.exists(subset_fpath):
+            # if there is no subset_indices file, that means the cache is the output of LDSCacher and
+            # we handle files to have only model state_dicts in files and have a seperate subset_indices file
+            for model in pretrained_models:
+                fpath=os.path.join(self.cache_dir, model)
+                assert os.path.isfile(fpath)
+                fdict=torch.load(fpath, map_location=self.device)
+                subset_indices.append(fdict["sample_indices"])
+                ckpt=fdict["model_state"]
+                torch.save(ckpt, fpath)
+            self.subset_ids=torch.stack(subset_indices, dim=0).to(self.device)
+            torch.save(subset_indices, subset_fpath)
+
+        else:
+            self.subset_ids=torch.load(subset_fpath)
+            for model in pretrained_models:
+                fpath=os.path.join(self.cache_dir, model)
+                assert os.path.isfile(fpath)
+                fdict=torch.load(fpath)
+        return
+
+    def get_test_datapoints(self, start, length):
+        targets=[]
+        samples=[]
+        for i in range(length):
+            x, y = self.test[start+i]
+            targets.append(y)
+            samples.append(x)
+        return torch.stack(samples).to(self.device), torch.tensor(targets, device=self.device)
+    
+    def __init__(self, train, test, model, pretrained_models, cache_dir, device="cuda"):
+        super().__init__(train,test)
+        if train.name != "AWA":
+            if isinstance(train.targets,list):
+                train.targets=torch.tensor(train.targets,device=device)
+            self.train_labels = train.targets.to(device)
+        self.device=device
+        self.cache_dir=cache_dir
+        self.process_cache_dir(pretrained_models)
+        self.train = train
+        self.test = test
+        self.scores = torch.empty(0, dtype=torch.float, device=device)
+        
+        self.quanda_metric=LinearDatamodelingMetric(
+            model=model,
+            train_dataset=train,
+            pretrained_models=pretrained_models,
+            subset_ids=self.subset_ids,
+            cache_dir=cache_dir
+            )
+
+        self.device = device
+
+    def __call__(self, xpl, start_index):
+        xpl.to(self.device)
+        if xpl.nelement() == 0: #to exit if xpl is empty
+            return
+        t_data,t_labels=self.get_test_datapoints(start_index, xpl.shape[0])
+        self.quanda_metric.update(explanations=xpl, test_targets=t_labels, test_data=t_data)
+        
+
+    def get_result(self, dir=None, file_name=None):
+        score=self.quanda_metric.compute()["score"]
+        resdict = {'metric': self.name, 'correlation_scores': self.quanda_metric.results, 'avg_score': score,}
+        if dir is not None:
+            self.write_result(resdict, dir, file_name)
+        return resdict
