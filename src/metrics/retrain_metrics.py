6b483b1cbad6df98df7ffbf5152485d914b2d54a
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-03-28 13:59:14 +0100

    fix lds bug

--- src/metrics/retrain_metrics.py
+++ src/metrics/retrain_metrics.py
@@ -404,10 +404,12 @@ class LinearDatamodelingScore(RetrainMetric):
         self.attribution_array = torch.empty(0,self.samples)
         self.n_test = None
         self.device = device
+        self.sample_indices=None
 
     def __call__(self, xpl, start_index=0):
         xpl=xpl.to(self.device)
         self.n_test = xpl.shape[0]
+        sample_indices=[]
         evalds = torch.cat([self.test[i][0].unsqueeze(dim=0) for i in range(start_index,start_index + xpl.shape[0])], dim=0).to(self.device)
         evalds_labels = torch.Tensor([self.test[i][1] for i in range(start_index,start_index + xpl.shape[0])]).long().to(self.device)
         attribution_array = torch.empty((xpl.shape[0], self.samples))
@@ -418,12 +420,16 @@ class LinearDatamodelingScore(RetrainMetric):
             retrained_model.load_state_dict(torch.load(model_path, map_location=torch.device(self.device))['model_state'])
             retrained_model.eval()
             cur_indices=torch.load(model_path, map_location=torch.device(self.device))['sample_indices']
+            if self.sample_indices is None:
+                sample_indices.append(cur_indices)
             attribution_array[:, i] = xpl[:, cur_indices].sum(dim=1).cpu().detach()
             logits = retrained_model(evalds)
             probs = F.log_softmax(logits, dim=1)
             binary_logits = probs.gather(1, evalds_labels.unsqueeze(1)).squeeze()
             #binary_logits = torch.log(correct_probs / (1-correct_probs+1e-10)) #added for numerical stability
             model_output_array[:, i] = binary_logits.cpu().detach()
+        if self.sample_indices is None:
+            self.sample_indices=torch.stack(sample_indices,dim=0).to(self.device)
         self.attribution_array=torch.cat((self.attribution_array, attribution_array), dim=0)
         self.model_output_array=torch.cat((self.model_output_array, model_output_array), dim=0)
 
