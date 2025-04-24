dc81ff3b856d5051dd1a11ad38a6efdbfc7b7857
Moritz Weckbecker <moritz.weckbecker@hhi.fraunhofer.de>
2025-04-24 15:31:35 +0200

    Updated ImageNet

--- src/dataset/ImageNet.py
+++ src/dataset/ImageNet.py
@@ -13,7 +13,9 @@ class ImageNet(tvImageNet):
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
-    inverse_transform = transforms.ToTensor()
+    inverse_transform = transforms.Compose([transforms.Normalize(mean=[0,0,0], std=[1/0.229, 1/0.224, 1/0.225]),
+                                            transforms.Normalize(mean=[-0.485, -0.456, -0.406], std=[1,1,1])
+    ])
 
     @staticmethod
     def get_index(item, split):
