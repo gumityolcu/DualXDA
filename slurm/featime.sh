888c5aa8494d4e7d5b79fe1b7553b6fb598aab61
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2025-06-06 18:54:58 +0200

    add script to compute forward pass time

---
+++ slurm/featime.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=explain
+#SBATCH --output=%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:4
+#SBATCH --mem=64G
+
+source "/etc/slurm/local_job_dir.sh"
+
+start=`date +%s`
+
+singularity \
+run \
+      --nv \
+      --bind ${HOME}/DualView/config_files:/mnt/config_files \
+      --bind ${HOME}/DualView/src:/mnt/src \
+      --bind ${HOME}/DualView/checkpoints:/mnt/checkpoints \
+      --bind ${DATAPOOL3}/datasets:/mnt/dataset \
+      --bind ${LOCAL_JOB_DIR}/outputs:/mnt/outputs \
+      --bind ${HOME}/DualView/cache:/mnt/cache \
+      ../singularity/featime.sif
