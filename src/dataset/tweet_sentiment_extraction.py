c0691aecd149bae0d02bd41d3ae9fabdb8a71169
Galip Ümit Yolcu <gumityolcu@gmail.com>
2025-10-07 14:17:41 +0200

    minor changes

--- src/dataset/tweet_sentiment_extraction.py
+++ src/dataset/tweet_sentiment_extraction.py
@@ -23,16 +23,26 @@ class TweetSentimentDatapoint(dict):
 class TweetSentimentDataset(torch.utils.data.Dataset):
     def __init__(self, split, device):
         dataset = load_dataset("mteb/tweet_sentiment_extraction")
-        tokenizer = AutoTokenizer.from_pretrained("herrerovir/gpt2-tweet-sentiment-model")
+        self.tokenizer = AutoTokenizer.from_pretrained("herrerovir/gpt2-tweet-sentiment-model")
         # Preprocess
         def preprocess(batch):
-            return tokenizer(batch["text"], truncation=True, padding="max_length", max_length=128)
+            return self.tokenizer(batch["text"], truncation=True, padding="max_length", max_length=128)
         self.device=device
         self.encoded = dataset.map(preprocess, batched=True)
         self.encoded = self.encoded.rename_column("label", "labels")
+        self.label_text=["negative", "neutral", "positive"]
         self.encoded.set_format("torch", columns=["input_ids", "attention_mask", "labels"])
         self.split = split
     
+    def get_string(self, id, skip_special_tokens=True):
+        id=int(id)
+        x = self.encoded[self.split][id]
+        # mask=x["attention_mask"]
+        # final_id=mask.sum()-1
+        x=x["input_ids"]
+        string=self.tokenizer.batch_decode(x,skip_special_tokens=skip_special_tokens)
+        return "".join(string)
+
     def __len__(self):
         return len(self.encoded[self.split])
 
