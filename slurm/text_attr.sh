8f68fcefb82573f7a49ce707a3b9a8c401ac31f3
Galip Ümit Yolcu <gumityolcu@gmail.com>
2025-10-12 15:29:00 +0200

    text_attr.sh

---
+++ slurm/text_attr.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=explain
+#SBATCH --output=%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:4
+#SBATCH --mem=64G
+
+source "/etc/slurm/local_job_dir.sh"
+
+start=`date +%s`
+
+mkdir -p ${LOCAL_JOB_DIR}/outputs
+
+singularity \
+run \
+      --nv \
+      --bind ${HOME}/DualView/src:/mnt/src \
+      --bind ${HOME}/DualView/explanations:/mnt/explanations \
+      --bind ${DATAPOOL3}/datasets:/mnt/dataset \
+      --bind ${LOCAL_JOB_DIR}/outputs:/mnt/outputs \
+      --bind ${HOME}/DualView/cache:/mnt/cache \
+      ../singularity/explain.sif --start 0 --length 2 --save_dir /mnt/outputs --dataset_name ag_news --xai_method $1
+cd ${LOCAL_JOB_DIR}
+tar -czf $1-output_data_${SLURM_JOB_ID}.tgz outputs
+cp $1-output_data_${SLURM_JOB_ID}.tgz ${SLURM_SUBMIT_DIR}
+
+end=`date +%s`
+runtime=$((end-start))
+echo "Runtime: $runtime"
+echo "In minutes: $(($runtime / 60))"
